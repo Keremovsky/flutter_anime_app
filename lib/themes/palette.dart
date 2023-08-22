2700ae4d302a4e9eeae4721c32b3ede8108bdfce
Kerem Okumus <96080839+Keremovsky@users.noreply.github.com>
2023-08-22 22:26:50 +0300

    app's main structure created

---
+++ lib/themes/palette.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  // colors
+  static const mainColor = Color.fromRGBO(245, 128, 51, 1);
+  static const background = Color.fromRGBO(25, 25, 25, 1);
+  static const white = Color.fromRGBO(248, 248, 248, 1);
+  static const grey = Color.fromRGBO(102, 102, 102, 1);
+}
