d09a1157f816f607ba9ce8bc62698f7dd6498835
Kerem Okumus <96080839+Keremovsky@users.noreply.github.com>
2023-09-18 15:25:13 +0300

    deleting lists feature added

--- lib/features/anime/screens/anime_list_screen.dart
+++ lib/features/anime/screens/anime_list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_anime_app/core/providers/state_notifier_providers/anime_lists_state_notifier.dart';
 import 'package:flutter_anime_app/core/utils/app_bar_back_button.dart';
 import 'package:flutter_anime_app/features/anime/controller/anime_controller.dart';
 import 'package:flutter_anime_app/features/anime/widgets/anime_list_view.dart';
@@ -6,6 +7,7 @@ import 'package:flutter_anime_app/models/anime_list.dart';
 import 'package:flutter_anime_app/models/pre_anime.dart';
 import 'package:flutter_anime_app/themes/palette.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class AnimeListScreen extends ConsumerStatefulWidget {
   final AnimeList animeList;
@@ -50,6 +52,79 @@ class _AnimeListScreenState extends ConsumerState<AnimeListScreen> {
         ),
         centerTitle: true,
         leading: const AppBarBackButton(),
+        actions: [
+          IconButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    elevation: 0,
+                    content: SizedBox(
+                      height: 120,
+                      child: Column(
+                        children: [
+                          Text(
+                            "Are you sure?",
+                            style: Theme.of(context).textTheme.displayLarge,
+                          ),
+                          const SizedBox(height: 10),
+                          Text(
+                            "This will delete list permanetly.",
+                            style: Theme.of(context).textTheme.displayMedium,
+                          ),
+                          const SizedBox(height: 20),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              ElevatedButton(
+                                onPressed: () async {
+                                  await ref
+                                      .read(animeControllerProvider.notifier)
+                                      .deleteAnimeList(
+                                          context, widget.animeList.name);
+
+                                  await ref
+                                      .read(animeListsStateNotifierProvider
+                                          .notifier)
+                                      .updateState();
+
+                                  if (mounted) {
+                                    context.pop();
+                                    context.pop();
+                                  }
+                                },
+                                child: Text(
+                                  "Yes",
+                                  style:
+                                      Theme.of(context).textTheme.displayLarge,
+                                ),
+                              ),
+                              ElevatedButton(
+                                onPressed: () {
+                                  context.pop();
+                                },
+                                child: Text(
+                                  "No",
+                                  style:
+                                      Theme.of(context).textTheme.displayLarge,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              );
+            },
+            icon: const Icon(
+              Icons.delete,
+              color: Palette.mainColor,
+            ),
+          ),
+        ],
       ),
       body: AnimeListView(animeList: widget.animeList),
     );
